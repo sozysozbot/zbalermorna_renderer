cf81666cfb7f80af2ad47e1e75010fdb6794bb64
sozysozbot <sozysozbot@gmail.com>
2016-10-14 23:49:36 +0900

    try

--- docs/script.js
+++ docs/script.js
@@ -3,17 +3,46 @@ onload = function() {
 };
 
 var WIDTH=42;
+var VSPACE=100;
 
 function draw() {
   var canvas = document.getElementById('c1');
   if ( ! canvas || ! canvas.getContext ) { return false; }
   var ctx = canvas.getContext('2d');
-  make_image(ctx,"b", 30,30);
-  make_image(ctx,"i", 30,30);
-  make_image(ctx,"r", 30+WIDTH,30);
-  make_image(ctx,"t", 30+WIDTH*2,30);
-  make_image(ctx,"i", 30+WIDTH*2,30);
+  output_syllable(ctx, "bi", 0,0);
+  output_syllable(ctx, "r",  1,0);
+  output_syllable(ctx, "ti", 2,0);
+  output_syllable(ctx, "_",  3,0);
+  output_syllable(ctx, "lo", 4,0);
+  output_syllable(ctx, "l",  5,0);
+  output_syllable(ctx, "di", 6,0);
+  output_syllable(ctx, "_",  7,0);
+ 
+  output_syllable(ctx, "b",  0,1);
+  output_syllable(ctx, "c",  1,1);
+  output_syllable(ctx, "d",  2,1);
+  output_syllable(ctx, "f",  3,1);
+  output_syllable(ctx, "g",  4,1);
+  output_syllable(ctx, "j",  5,1);
+  output_syllable(ctx, "k",  6,1);
+  output_syllable(ctx, "l",  7,1);
+  output_syllable(ctx, "m",  8,1);
+  output_syllable(ctx, "n",  9,1);
+  output_syllable(ctx, "p", 10,1);
+  output_syllable(ctx, "r", 11,1);
+  output_syllable(ctx, "s", 12,1);
+  output_syllable(ctx, "t", 13,1);
+  output_syllable(ctx, "v", 14,1);
+  output_syllable(ctx, "x", 15,1);
+  output_syllable(ctx, "z", 16,1);
 
+}
+
+function output_syllable(ctx, str, m,n)
+{
+	for(var i=0; i<str.length; i++) {
+		make_image(ctx,str.charAt(i), 30+WIDTH*m, 30+VSPACE*n);
+	}
 }
 
 function make_image(ctx,chr,x,y)
