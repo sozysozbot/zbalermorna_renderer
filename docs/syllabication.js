680f5dab47560d6305148bc536b8e5ccac7931c7
sozysozbot <sozysozbot@gmail.com>
2016-10-21 23:21:59 +0900

    define function identify_cmene

--- docs/syllabication.js
+++ docs/syllabication.js
@@ -1,4 +1,42 @@
-﻿function split_into_syllables(str)
+﻿var REQUIRE_DOTSIDE = true;
+// identify cmene and mark it as such
+function identify_cmene(str)
+{
+	var ans = [];
+	if(!REQUIRE_DOTSIDE) {
+		/* FIXME */
+	} else {
+		//loops are required to handle something like ".loj.ban."
+		while(str.indexOf(".") + 1) { //after finding a period
+			if(ans[ans.length - 1] && ans[ans.length - 1].fadni) {
+				ans[ans.length - 1].fadni += str.slice(0,str.indexOf("."));
+			} else {
+				ans[ans.length] = {"fadni": str.slice(0,str.indexOf("."))};
+			}
+			
+			str = str.slice(str.indexOf("."));
+			var match = str.match(/^\.\s*[abcdefghijklmnopqrstuvwxyz]*[bcdfghjklmnprstvxz]\s*\./);
+			if(!match){
+				str = str.slice(1); //skip the period
+				ans[ans.length - 1].fadni += ".";
+				continue;
+			} else {
+				ans[ans.length] = {"cmene": match[0]};
+				str = str.slice(match[0].length - 1); //leave the period
+			}
+		}
+		if(ans[ans.length - 1] && ans[ans.length - 1].fadni) {
+			ans[ans.length - 1].fadni += str;
+		} else {
+			ans[ans.length] = {"fadni": str};
+		}
+	}
+	
+	return ans;
+}
+
+// main parser
+function split_into_syllables(str)
 {
 	str = str.replace(/['’]/g,"h");
 	var arr = [];
