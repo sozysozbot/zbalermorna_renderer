e84f56f3969d8d0e8253a13eecb0713a50560098
sozysozbot <sozysozbot@gmail.com>
2016-10-22 14:54:32 +0900

    hopefully implement correct cmene parsings

--- docs/syllabication.js
+++ docs/syllabication.js
@@ -90,6 +90,17 @@ function split_into_bases(str)
 }
 
 
+/* 
+	conforms to BPFK Decision "ban on semivowels preceded by a consonant"
+	thus: 
+		tciuauas = tci,ŭa,ŭas -> ["t","c","i","w","a","w","a","s"]
+		tciauas = tci,au,as -> ["t","c","i","au","a","s"]
+		iauas = ĭa,ŭas -> ["q","a","w","a","s"]
+		nuiok = nu,ĭok -> ["n","u","q","o","k"]
+	note:
+		meiin -> ["m","ei","i","n"]
+		me,iin -> ["m","e","q","i","n"]
+*/
 function split_cmene_into_symbols(str)
 {
 	var match = str.match(/[abcdefghijklmnopqrstuvwxyz]*[bcdfghjklmnprstvxz]/);
@@ -98,11 +109,48 @@ function split_cmene_into_symbols(str)
 	}
 	var cmene = match[0];
 	
-	// FIXME: diphthong & mandatory spacing not handled
-	var arr = cmene.split("").map(function(c){
-		if(["a","e","i","o","u","y"].indexOf(c) + 1) return c+"_sepli";
-		return c;
-	});
-	
-	return arr;
+	var arr2 = [];
+	var str = cmene;
+	while(str.length) {
+		if("bcdfghjklmnprstvxz".indexOf(str.charAt(0))) { //consonant
+			arr2[arr2.length] = str.charAt(0);
+			//falling diphthongs -> parse
+			if(["au","ai","ei","oi"].indexOf(str.slice(1,3)) !== -1) {
+				arr2[arr2.length] = str.slice(1,3) + "_sepli";
+				str = str.slice(3);
+				continue;
+			} else if(["a","e","i","o","u","y"].indexOf(str.charAt(1))) {
+				arr2[arr2.length] = str.charAt(1) + "_sepli";
+				str = str.slice(2);
+				continue;
+			} else {
+				str = str.slice(1);
+				continue;
+			}
+		} else { //vowel-syllable; allows iV and uV
+			if(["au","ai","ei","oi"].indexOf(str.slice(0,2)) !== -1) {
+				arr2[arr2.length] = str.slice(0,2) + "_sepli";
+				str = str.slice(2);
+				continue;
+			} else if(["i","u","q","w"].indexOf(str.charAt(0)) !== -1   // iai -> qai; iaia -> qaqa
+				   && ["ai","ei","oi","au"].indexOf(str.slice(1,3)) !== -1
+				   && ["a","e","i","o","u","y"].indexOf(str.charAt(3) === -1) {
+				arr2[arr2.length] = {"i":"q","q":"q","u":"w","w":"w"}[str.charAt(0)];
+				arr2[arr2.length] = str.slice(1,3) + "_sepli";
+				str = str.slice(3);
+				continue;
+			} else if(["i","u","q","w"].indexOf(str.charAt(0)) !== -1
+				   && ["a","e","i","o","u","y"].indexOf(str.charAt(1)) !== -1) {
+				arr2[arr2.length] = {"i":"q","q":"q","u":"w","w":"w"}[str.charAt(0)];
+				arr2[arr2.length] = str.charAt(1) + "_sepli";
+				str = str.slice(2);
+				continue;
+			} else {
+				arr2[arr2.length] = str.charAt(0) + "_sepli";
+				str = str.slice(1);
+				continue;
+			}
+		}
+	}
+	return arr2;
 }
